21eae41e0d9ccea6437d9ab07ce2709070b49a42
funtower <huangtao2@yusys.com.cn>
2019-09-15 13:23:39 +0800

    # thrid commit

--- geek/src/main/java/geekcompetition/liuhy/LiuHY.java
+++ geek/src/main/java/geekcompetition/liuhy/LiuHY.java
@@ -3,7 +3,7 @@ package geekcompetition.liuhy;
 public class LiuHY {
 
 	public static void main(String[] args) {
-		System.out.println("oh yeah fuck");
+		System.out.println("oh yeah fuck 有");
 
 	}
 
